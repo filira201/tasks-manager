ebba2fd3fb9f891bf215e10815c708af176193c7
Egor <egor297198@mail.ru>
2025-10-31 15:44:58 +0400

    Update: add data-testid

--- frontend/src/pages/task/ui/TaskPage.tsx
+++ frontend/src/pages/task/ui/TaskPage.tsx
@@ -3,7 +3,7 @@ import { TaskPageContent } from "@/widgets/TaskPage";
 export const TaskPage = () => {
   return (
     <div className="min-h-screen h-full">
-      <h1 className="mb-10 font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl">Редактирование Задачи</h1>
+      <h1 data-testid="task-page-title" className="mb-10 font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl">Редактирование задачи</h1>
       <TaskPageContent />
     </div>
   );
--- frontend/src/pages/taskCreate/ui/TaskCreatePage.tsx
+++ frontend/src/pages/taskCreate/ui/TaskCreatePage.tsx
@@ -3,7 +3,7 @@ import { TaskCreatePageContent } from "@/widgets/TaskCreate";
 export const TaskCreatePage = () => {
   return (
     <div className="min-h-screen h-full">
-      <h1 className="mb-10 font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl">Создать задачу</h1>
+      <h1 data-testid="task-create-page-title" className="mb-10 font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl">Создать задачу</h1>
       <TaskCreatePageContent />
     </div>
   );
