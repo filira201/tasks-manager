297ba9a97b6e0400dd2c78ceebf465c8ff9f8ec8
Egor <egor297198@mail.ru>
2025-10-31 15:45:08 +0400

    Add: add tests for routes

--- frontend/src/app/routes/tests/AppRouter.test.tsx
+++ frontend/src/app/routes/tests/AppRouter.test.tsx
@@ -13,8 +13,7 @@ beforeEach(() => {
 });
 
 describe("тестирование страниц (AppRouter)", () => {
-  test("если пользователь на главной странице, то должен быть заголовок h1 с текстом 'Задачи'", () => {
-    //Подготовка
+  test("если пользователь на главной странице (/), то должен быть заголовок h1 с текстом 'Задачи'", () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
         <Providers>
@@ -23,10 +22,52 @@ describe("тестирование страниц (AppRouter)", () => {
       </MemoryRouter>
     );
 
-    //Проверка
     const title = screen.getByTestId("tasks-page-title");
     expect(title).toBeInTheDocument();
     expect(title.tagName).toBe("H1");
     expect(title).toHaveTextContent("Задачи");
+  });
+
+  test("если пользователь на странице задачи (/task/1), то должен быть заголовок h1 с текстом 'Редактирование задачи'", () => {
+    render(
+      <MemoryRouter initialEntries={["/task/1"]}>
+        <Providers>
+          <AppRouter />
+        </Providers>
+      </MemoryRouter>
+    );
+
+    const title = screen.getByTestId("task-page-title");
+    expect(title).toBeInTheDocument();
+    expect(title.tagName).toBe("H1");
+    expect(title).toHaveTextContent("Редактирование задачи");
+  });
+
+  test("если пользователь на странице создания задачи (/task/new), то должен быть заголовок h1 с текстом 'Создать задачу'", () => {
+    render(
+      <MemoryRouter initialEntries={["/task/new"]}>
+        <Providers>
+          <AppRouter />
+        </Providers>
+      </MemoryRouter>
+    );
+
+    const title = screen.getByTestId("task-create-page-title");
+    expect(title).toBeInTheDocument();
+    expect(title.tagName).toBe("H1");
+    expect(title).toHaveTextContent("Создать задачу");
+  });
+
+  test("если пользователь переходит на неизвестный маршрут, то должен быть редирект на '/'", async () => {
+    render(
+      <MemoryRouter initialEntries={["/unknown"]}>
+        <Providers>
+          <AppRouter />
+        </Providers>
+      </MemoryRouter>
+    );
+
+    const titles = await screen.findAllByText("Задачи");
+    expect(titles.length).toBeGreaterThan(0);
   });
 });
