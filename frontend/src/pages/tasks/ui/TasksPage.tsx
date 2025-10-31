85b3041feb91d571de160e69dc1ef54af148d38a
Egor <egor297198@mail.ru>
2025-10-31 14:08:33 +0400

    Add: add test for main page

--- frontend/src/pages/tasks/ui/TasksPage.tsx
+++ frontend/src/pages/tasks/ui/TasksPage.tsx
@@ -3,7 +3,9 @@ import { TasksPageContent } from "@/widgets/Tasks";
 export const TasksPage = () => {
   return (
     <div className="min-h-screen h-full">
-      <h1 className="mb-10 font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl">Задачи</h1>
+      <h1 data-testid="tasks-page-title" className="mb-10 font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+        Задачи
+      </h1>
       <TasksPageContent />
     </div>
   );
