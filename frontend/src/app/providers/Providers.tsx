85d5a36fccc463b59f9324e2f2c886b4454b9e6b
Egor <egor297198@mail.ru>
2025-11-02 19:13:16 +0400

    Add: add ToastProvider

--- frontend/src/app/providers/Providers.tsx
+++ frontend/src/app/providers/Providers.tsx
@@ -1,4 +1,4 @@
-import { HeroUIProvider } from "@heroui/react";
+import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import type { ReactNode } from "react";
 import { Provider } from "react-redux";
 
@@ -11,6 +11,18 @@ type Props = {
 export const Providers = ({ children }: Props) => {
   return (
     <HeroUIProvider>
+      <ToastProvider
+        placement="top-right"
+        toastOffset={60}
+        toastProps={{
+          radius: "md",
+          timeout: 2000,
+          shouldShowTimeoutProgress: true,
+          classNames: {
+            closeButton: "opacity-100 absolute right-4 top-1/2 -translate-y-1/2",
+          },
+        }}
+      />
       <Provider store={store}>{children}</Provider>
     </HeroUIProvider>
   );
